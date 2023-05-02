8404380ce683acd43d2e50b2beb4d8c2dcbd721a
leftra123 <eric.aguayo@inacapmail.cl>
2023-05-02 15:59:01 -0400

    modificacion de DB para conectar con postgresql

--- colegio_transportes/settings.py
+++ colegio_transportes/settings.py
@@ -81,10 +81,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ['POSTGRES_DATABASE'],
-        'USER': os.environ['POSTGRES_USER'],
-        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
-        'HOST': os.environ['POSTGRES_HOST'],
-        'PORT': os.environ['POSTGRES_PORT'],
     }
 }
 
